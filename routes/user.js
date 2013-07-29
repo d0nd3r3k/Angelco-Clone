7028981407f1a7ac187caf134e263489f9052d1e
DonaldDerek <donaldderek.haddad@gmail.com>
2013-07-29 11:52:37 +0200

    Added MongoDB

--- routes/user.js
+++ routes/user.js
@@ -2,19 +2,44 @@
 /*
  * Users Routes.
  */
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/arabangels');
+
+var Subscriber = mongoose.model('subscribers',
+		 { name: String,
+		   email: String,
+		   country: String,
+		   haveInvested: String,
+		   isInterested: String 
+		  });
+
 
 exports.subscribe = function(req, res){
-   console.log(req.body.name);
-   console.log(req.body.email);
-   console.log(req.body.country);
+   
+   var data = { name: req.body.name,
+   				email: req.body.email,
+   				country: req.body.country };
 
    if(req.body.haveInvested !== undefined){
-   		console.log(req.body.haveInvested);
+   		data.haveInvested = req.body.haveInvested;
+   } else {
+   		data.haveInvested = "false";
    }
 
    if(req.body.isInterested !== undefined){
-   		console.log(req.body.isInterested);
+   		data.isInterested = req.body.isInterested;
+   } else {
+   		data.isInterested = "false";
    }
+
+   var subscribe = new Subscriber(data);
+	
+	Subscriber.findOneAndUpdate({email: data.email}, data, {upsert:true}, function(err){
+		if(err){
+			console.log(err);
+		}
+	});
+   
    res.writeHead(200);
    res.end();
 }
