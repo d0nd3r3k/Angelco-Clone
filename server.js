37ccd7210714c8e0ec82efa5d741ee29b541bc9b
DonaldDerek <donaldderek.haddad@gmail.com>
2013-10-15 11:56:43 +0300

    config

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var express = require('express')
 
 // Load configurations
 // if test env, load example file
-var env = process.env.NODE_ENV || 'development'
+var env = process.env.NODE_ENV || 'production'
   , config = require('./config/config')[env]
   , mongoose = require('mongoose')
 
